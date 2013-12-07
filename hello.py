7c7fe06cb39643d09d5e1cbae811ff4c7712b8b0
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-12-06 21:22:58 -0800

    Not sure what it's doing

--- hello.py
+++ hello.py
@@ -1,20 +1,25 @@
-from flask import Flask, request, make_response
-from flask_sockets import Sockets
-import Queue
-import time
+import  collections
+from    flask import Flask, request, make_response
+from    flask_sockets import Sockets
+import  Queue
+import  time
 
-messages = Queue.Queue()
+messages_by_subscriber = collections.defaultdict(Queue.Queue)
 
 app = Flask(__name__)
 sockets = Sockets(app)
 
-@sockets.route('/echo')
+@sockets.route('/echo/')
 def echo_socket(ws):
+    subscriber_id = ws.receive()
+    print("echo_socket: initial message: {!r}".format(subscriber_id))
     while True:
-        ws.send(messages.get())
+        m = messages_by_subscriber[subscriber_id].get()
+        print("echo_socket: m is {}".format(m))
+        ws.send(m)
 
-@app.route('/update/', methods=['POST'])
-def update():
-    print("Ayup")
-    messages.put(request.form['stuff'])
-    return make_response('groovy', 204)
+@app.route('/update/<subscriber_id>/', methods=['POST'])
+def update(subscriber_id):
+    print("update: subscriber_id is {}".format(subscriber_id))
+    messages_by_subscriber[subscriber_id].put(request.form['stuff'])
+    return make_response(subscriber_id, 204)
